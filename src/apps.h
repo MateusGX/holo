adcbcb84bac5f98e1955d7f876dc3b14926a4b82
MateusGX <mateusam.ofc@outlook.com>
2024-08-23 18:30:37 -0300

    removed unnecessary files

--- src/apps.h
+++
@@ -1,9 +0,0 @@
-#ifndef APPS_H
-#define APPS_H
-// #include "menu/menu.h"
-
-// extern Menu subMenuApplications("Apps");
-
-// #define APP_DEFINE(name) subMenuApplications.addItem(new CallbackMenuItem(name, &subMenu##name, &subMenuApplications))
-
-#endif
